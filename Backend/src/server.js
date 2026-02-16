63d510d723bbc306b94c99c05480e8381b0d948b
Ayushrai0 <144903405+Ayushrai0@users.noreply.github.com>
2026-02-16 20:22:27 +0530

    some change

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,9 +4,6 @@ import cors from "cors";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-import { inngest, functions } from "./lib/inngest.js";
-
-
 
 const app = express();
 
